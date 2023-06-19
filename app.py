39a935cec28d09cca2555d07cbd8a1044023e9a0
Van Hien <hiench2001@gmail.com>
2023-06-19 14:31:31 -0600

    set and get cookies

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, make_response
 from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('student.html')
+    return render_template('index.html')
 
-@app.route('/result', methods=['POST', 'GET'])
-def result():
+@app.route('/setcookie', methods=['POST', 'GET'])
+def setcookie():
     if request.method == 'POST':
-        result = request.form
-        return render_template('result.html', result=result)
+        user = request.form['nm']
         
+    resp = make_response(render_template('readcookie.html'))
+    resp.set_cookie("userID", user)
+    return resp
+
+@app.route('/getcookie')
+def getcookie():
+   name = request.cookies.get('userID')
+   return '<h1>welcome '+ name +'</h1>'
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
