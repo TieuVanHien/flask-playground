d3ff9a4784188c1b1fe042d7513e85d47bdc8bee
Van Hien <hiench2001@gmail.com>
2023-06-19 11:21:41 -0600

    simple login route

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Main Page</p>"
+@app.route('/')
+def main():
+    return 'Hello'
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route('/success/<name>')
+def success(name):
+    return f'{escape(name)} successfully logined'
 
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name = user))
+    else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+        
 if __name__ == "__main__":
     app.run(debug=True)
