e4415e561deeab707e0d208a42f87944e0028548
Van Hien <hiench2001@gmail.com>
2023-06-19 15:21:01 -0600

    redo sending data and store in cookies

--- app.py
+++ app.py
@@ -7,20 +7,24 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/setcookie', methods=['POST', 'GET'])
+@app.route('/setcookie', methods=['GET', 'POST'])
 def setcookie():
     if request.method == 'POST':
         user = request.form['nm']
-        
+        sub = request.form['sub']
+        age = request.form['age']
     resp = make_response(render_template('readcookie.html'))
     resp.set_cookie("userID", user)
+    resp.set_cookie("subject", sub)
+    resp.set_cookie("age", age)
     return resp
 
 @app.route('/getcookie')
 def getcookie():
-   name = request.cookies.get('userID')
-   return '<h1>welcome '+ name +'</h1>'
-
-    
+    name = request.cookies.get('userID')
+    sub = request.cookies.get('subject')
+    age = request.cookies.get('age')
+    return '<1h> You are ' + name + ' and you are ' + age + ' and your favorite subject is ' + sub + '</h1>'   
+     
 if __name__ == "__main__":
     app.run(debug=True)
